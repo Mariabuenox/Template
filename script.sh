9fe08cbbe236c9ff0807b17b65b4e76eb097b08c
Maria Bueno <159018444+Mariabuenox@users.noreply.github.com>
2024-02-05 10:00:42 -0300

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,11 @@
+sudo apt update
+sudo apt upgrade 
+sudo apt install mysql-server 
+sudo apt install php
+sudo apt install phpmyadmin
+sudo apt install apache2
+sudo ./ mariaclara
+chmd 777 mariaclara 
+
+
+
